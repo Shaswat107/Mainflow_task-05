67238ad76e71755e420e7ab5a495cd65a536da3d
Shaswat107 <shaswatagrahari0@gmail.com>
2024-08-22 22:39:10 +0530

    commit

---
+++ Calculator_React/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import CAlculator from './components/Calculator';
+
+function App() {
+  return (
+    <div>
+     <CAlculator />
+    </div>
+  )
+}
+
+export default App
